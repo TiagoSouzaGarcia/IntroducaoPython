906dadeb8f2350dd24d7ac5399cbeca24e4fa636
Tiago Souza Garcia <garciats@saude.sc.gov.br>
2024-06-03 17:56:26 -0300

    Solução desafio 1

--- src/main/desafio-lista.py
+++ src/main/desafio-lista.py
@@ -1,2 +1,37 @@
+"""1. Escreva um código que lê a lista abaixo e faça:
+
 lista = [16, 14, 63, 65, 17, 99, 70, 11, 20, 48, 79, 32, 17, 89, 12, 25, 66]
+
+    A leitura do tamanho da lista
+    A leitura do maior e menor valor
+    A soma dos valores da lista
+
+Ao final exiba uma mensagem dizendo:
+
+"A lista possui [tam] números em que o maior número é [maior] e o menor número é [menor].
+A soma dos valores presentes nela é igual a [soma]"""
+
+# Solução 1
+lista = [16, 14, 63, 65, 17, 99, 70, 11, 20, 48, 79, 32, 17, 89, 12, 25, 66]
+tam = len(lista)
+soma = sum(lista)
+maior = max(lista)
+menor = min(lista)
+
+print(f"A lista possui {tam} números em que o maior número é {maior} e o menor número é {menor}. A soma dos valores é igual a {soma}")
+
+# -------------------------------------------
+
+"""2. Escreva uma função que gere a tabuada de um número inteiro de 1 a 10, de acordo com a escolha da pessoa usuária. Como exemplo, para o número 7, a tabuada deve ser mostrada no seguinte formato:
+
+Tabuada do 7:
+7 x 0 = 0
+7 x 1 = 7
+[...]
+7 x 10 = 70
+"""
+
+# Solução 2
+
+
 
